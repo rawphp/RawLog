7fad3816d1799d1fb911f1fb97b748d02b17bc72
Tom Kaczocha <tom@rawphp.org>
2014-10-01 23:41:04 +1000

    Fixed timezone in test.

--- tests/unit/lib/formatters/ErrorLogFormatterTest.php
+++ tests/unit/lib/formatters/ErrorLogFormatterTest.php
@@ -86,6 +86,7 @@ class ErrorLogFormatterTest extends \PHPUnit_Framework_TestCase
     {
         $record = new ErrorLogRecord( Log::LEVEL_ERROR, 'This is the error message' );
         $date  = new \DateTime( );
+        $date->setTimezone( new \DateTimeZone( 'EST' ) );
         $date->setDate( 2014, 10, 01 );
         $date->setTime( 23, 14, 55 );
 
